de7a9d42bb703824b00db932c27de8b4a3ff61eb
ezQel <ezekiel3499@gmail.com>
2020-10-08 09:00:32 +0300

    feat(props): added logo components

--- src/app/props/props.module.ts
+++ src/app/props/props.module.ts
@@ -2,16 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PropsRoutingModule } from './props-routing.module';
 import { KesPipe } from './kes.pipe';
-
-
+import { LogoComponent } from './logo/logo.component';
+import { SmallLogoComponent } from './small-logo/small-logo.component';
 
 @NgModule({
   declarations: [
     KesPipe,
+    LogoComponent,
+    SmallLogoComponent,
   ],
   imports: [
     CommonModule,
     PropsRoutingModule,
+  ],
+  exports: [
+    KesPipe,
+    LogoComponent,
+    SmallLogoComponent,
   ]
 })
 export class PropsModule { }
---
+++ src/app/props/small-logo/small-logo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-small-logo',
+  templateUrl: './small-logo.component.html',
+  styleUrls: ['./small-logo.component.css']
+})
+export class SmallLogoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
