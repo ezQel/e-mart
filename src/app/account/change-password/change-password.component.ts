7abc2d73540a932a1e8ebf811f93cd3646a65196
ezQel <ezekiel3499@gmail.com>
2020-10-09 22:36:01 +0300

    feat(account): created change passwod component

---
+++ src/app/account/change-password/change-password.component.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/user.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Title } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-change-password',
+  templateUrl: './change-password.component.html',
+  styleUrls: ['./change-password.component.css']
+})
+export class ChangePasswordComponent implements OnInit {
+  passwordForm = new FormGroup({
+    newPassword: new FormControl(''),
+  });
+
+  passwordVisible = false;
+  authRequired;
+
+  constructor(private router: Router, private userService: UserService, private snackBar: MatSnackBar, private title: Title) { }
+
+  ngOnInit(): void {
+    this.title.setTitle('Fechi | Change Password');
+  }
+
+  togglePasswordVisibility(pwdField): void {
+    if (this.passwordVisible) {
+      pwdField.type = 'password';
+      this.passwordVisible = false;
+    }
+    else {
+      pwdField.type = 'text';
+      this.passwordVisible = true;
+    }
+  }
+
+  changePassword(): void{
+    if (this.passwordForm.valid) {
+      this.userService.auth.currentUser
+      .then(
+        user => {
+          user.updatePassword(this.passwordForm.get('newPassword').value)
+          .then(
+            (reason) => {
+              this.authRequired = reason;
+            }
+          );
+          if (!this.authRequired) {
+            this.snackBar.open('Password change successful', 'DISMISS', { duration: 3000 });
+            this.router.navigate(['account']);
+          }
+        }
+      );
+    }
+
+  }
+
+  logoutToChangePassword(): void {
+    this.userService.auth.signOut();
+    this.router.navigate(['login']);
+    // TODO redirect user back to change password after login
+  }
+
+}
