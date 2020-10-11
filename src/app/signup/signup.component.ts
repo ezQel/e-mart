054919b74d9fc280685f6a645f35a950d7ab6f91
ezQel <ezekiel3499@gmail.com>
2020-10-11 23:01:28 +0300

    (signup): added signup page

---
+++ src/app/signup/signup.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+  passwordVisible = false;
+  errorMsg;
+
+  signUpForm = new FormGroup({
+    email : new FormControl('', [Validators.email]),
+    password : new FormControl(''),
+  }
+  );
+
+  constructor(private userService: UserService) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  togglePasswordVisibility(pwdField): void {
+    if (this.passwordVisible) {
+      pwdField.type = 'password';
+      this.passwordVisible = false;
+    }
+    else {
+      pwdField.type = 'text';
+      this.passwordVisible = true;
+    }
+  }
+
+  signUp(): void {
+    if (this.signUpForm.valid){
+      const email = this.signUpForm.get('email').value;
+      const password = this.signUpForm.get('password').value;
+
+      this.userService.auth
+        .createUserWithEmailAndPassword(email, password)
+        .then(cr => {
+          this.userService.db.collection('users').doc<any>(cr.user.uid).set(
+            {
+              userType: 'regular'
+            }
+          );
+          cr.user.updateProfile(
+            {
+              displayName: cr.user.email.split('@').shift()
+            }
+          );
+        }).catch((error) => {
+          this.errorMsg = error.message;
+        });
+    }
+  }
+
+}
