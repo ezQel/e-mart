33ffb53b3c8d0ce2240b30264bd52055ca3fbd60
ezQel <ezekiel3499@gmail.com>
2020-10-17 16:23:29 +0300

    feat(login): added password reset page

---
+++ src/app/login/reset-password/reset-password.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/user.service';
+import { Title } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-reset-password',
+  templateUrl: './reset-password.component.html',
+  styleUrls: ['./reset-password.component.css']
+})
+export class ResetPasswordComponent implements OnInit {
+
+  resetCodeForm = new FormGroup({
+    resetCode: new FormControl(''),
+    newPwd: new FormControl('')
+  });
+
+  passwordVisible = false;
+
+  constructor(private router: Router, private userService: UserService, private title: Title) { }
+
+  ngOnInit(): void {
+    this.title.setTitle('Fechi | Reset Password');
+  }
+
+  togglePasswordVisibility(pwdField): void {
+    if (this.passwordVisible) {
+      pwdField.type = 'password';
+      this.passwordVisible = false;
+    }
+    else {
+      pwdField.type = 'text';
+      this.passwordVisible = true;
+    }
+  }
+
+  resetUserPassword(): void {
+    if (this.resetCodeForm.valid){
+      this.userService
+      .auth
+      .confirmPasswordReset(
+        this.resetCodeForm.get('resetCode').value,
+        this.resetCodeForm.get('newPwd').value
+      )
+      .then(
+        f => this.router.navigate([''])
+      );
+    }
+
+  }
+
+}
