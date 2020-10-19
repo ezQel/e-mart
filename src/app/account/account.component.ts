7c4519d16b77ddd587dc05d7bb427ea34fd55d1a
ezQel <ezekiel3499@gmail.com>
2020-10-19 18:00:16 +0300

    feat(account): account module complete

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
 import { Router } from '@angular/router';
 import { Title } from '@angular/platform-browser';
+import { Address } from '../data/address';
 
 @Component({
   selector: 'app-account',
@@ -9,19 +10,12 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./account.component.css']
 })
 export class AccountComponent implements OnInit {
-  email: string;
-  name: string;
-  address: string;
   editAddress: boolean;
 
-  constructor(private userService: UserService, private router: Router, private title: Title) { }
+  constructor(public userService: UserService, private router: Router, private title: Title) { }
 
   ngOnInit(): void {
     this.title.setTitle('Fechi | Account');
-
-    this.email = this.userService.currentUser.email;
-    this.name = this.userService.currentUser.fullName;
-    this.address = (this.userService.userAddress) ? this.userService.userAddress.streetAddress : '- - not set - -';
   }
 
   toggleEditAddress(): void {
