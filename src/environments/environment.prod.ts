7945ec1c22fb9993a5c4b459db234eb8d23ec1c5
ezQel <ezekiel3499@gmail.com>
2020-10-20 08:37:22 +0300

    ci(environment): added fierbase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyC1nZbBukybn3V-MFBWNkexHv6f_vpUvPo',
+    authDomain: 'mart-c1d66.firebaseapp.com',
+    databaseURL: 'https://mart-c1d66.firebaseio.com',
+    projectId: 'mart-c1d66',
+    storageBucket: 'mart-c1d66.appspot.com',
+    messagingSenderId: '1057202021333',
+    appId: '1:1057202021333:web:7ced343b0bf719c922935c',
+    measurementId: 'G-MGTH1JWG7P'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyC1nZbBukybn3V-MFBWNkexHv6f_vpUvPo',
+    authDomain: 'mart-c1d66.firebaseapp.com',
+    databaseURL: 'https://mart-c1d66.firebaseio.com',
+    projectId: 'mart-c1d66',
+    storageBucket: 'mart-c1d66.appspot.com',
+    messagingSenderId: '1057202021333',
+    appId: '1:1057202021333:web:7ced343b0bf719c922935c',
+    measurementId: 'G-MGTH1JWG7P'
+  }
 };
 
 /*
